16e7693a5143c4b7272319a07cfc7a0419608c72
Ross Mather <ramather96@gmail.com>
2022-01-30 20:44:17 -0500

    api call fixes

--- pokemon-generator/src/components/PokemonCard.js
+++ pokemon-generator/src/components/PokemonCard.js
@@ -1,68 +1,65 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react/cjs/react.development";
 
 const PokemonCard = (props) => {
-    console.log(props);
-    console.log(props.pokemon.rarity)
+  // eslint-disable-next-line no-unused-vars
+  function displayDescription(description) {
+    if (description !== "") return <p>{description}</p>;
+  }
 
-    // eslint-disable-next-line no-unused-vars
-    function displayDescription(description){
-      if(description !== "")return <p>{description}</p>
-    }
-    
-    return(
-        
+  return (
     <div className={props.pokemon.type}>
-        <section className='title-row'>
-            <p className='rarity'></p>
-            <h1 className='name'>{props.pokemon.name}</h1>
-            <p className='health'>{props.pokemon.hp}</p>
+      <section className="title-row">
+        <p className="rarity"></p>
+        <h1 className="name">{props.pokemon.name}</h1>
+        <p className="health">{props.pokemon.hp}</p>
+        <img alt="" className="element-icon" src={props.pokemon.icon}></img>
+      </section>
+      <section className="character-img">
+        <img alt="" src={props.pokemon.img}></img>
+      </section>
+      <section className="character-meta">
+        <p>
+          {props.pokemon.shape} Pokemon. Weight: {props.pokemon.weight}{" "}
+        </p>
+      </section>
+      
+      {props.pokemon.abilities.map((ability) => (
+        <section className="character-ability">
+          <span className="ability-cost">
+           
             <img alt="" className="element-icon" src={props.pokemon.icon}></img>
-
-        </section>
-        <section className="character-img">
-          <img alt="" src={props.pokemon.img}></img>
-        </section>
-        <section className="character-meta">
-          <p>{props.pokemon.meta}</p>
-        </section>
-        {(props.pokemon.abilities).map(ability =>(
-          <section className='character-ability'>
-              {console.log(ability.ability)}
-            <span className="ability-cost">
-              {/* {(ability.icons).map(icon =>(
-                <img alt=""  className="element-icon"  src={icon}></img>
-              ))} */}
-            </span>
-            <span className="ability-description">
-            <span className="ability-name">{ability.ability.name}</span>
-            {/* <displayDescription description={props.description} /> */}
           </span>
-          {/* <span className="ability-damage">{ability.damage}</span> */}
-
-          </section>
-        ))}
-       <section className="character-stats">
-          <span className="character-stat">
-            <p>weakness</p>
-            <img alt="" className="element-icon" src={props.pokemon.weakness}></img>
+          <span className="ability-description">
+            <span className="ability-name">{ability.name}</span>
+            
           </span>
-          <span className="character-stat">
-            <p>resistence</p>
-          </span>{" "}
-          <span className="character-stat">
-            <p>retreat cost</p>
-            <img alt="" className="element-icon"  src={props.pokemon.cost}></img>
-          </span>
-          </section>
-          <section className="character-description">
-            <p>{props.pokemon.description}</p>
+          {/* <span className="ability-damage">{ability.description}</span> */}
         </section>
-        <section> </section>
-
+      ))}
+      <section className="character-stats">
+        <span className="character-stat">
+          <p>weakness</p>
+          <img
+            alt=""
+            className="element-icon"
+            src={props.pokemon.weakness}
+          ></img>
+        </span>
+        <span className="character-stat">
+          <p>resistence</p>
+        </span>{" "}
+        <span className="character-stat">
+          <p>retreat cost</p>
+          <img alt="" className="element-icon" src={props.pokemon.cost}></img>
+        </span>
+      </section>
+      <section className="character-description">
+        <p>{props.pokemon.description}</p>
+      </section>
+      <section> </section>
     </div>
-    )
-    
-
-}
+  );
+};
 
 export default PokemonCard;
--- pokemon-generator/src/components/PokemonGenerator.js
+++ pokemon-generator/src/components/PokemonGenerator.js
@@ -3,58 +3,79 @@ import axios, { Axios } from "axios";
 import PokemonCard from "./PokemonCard";
 
 const PokemonGenerator = () => {
+  const [pokemonName, setPokemonName] = useState("");
 
-    const [pokemonName, setPokemonName]=useState('')
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    type: "",
+    icon: "",
+    cost: "./normal.png",
+    weakness: "./water.png",
+    weight: "",
+    description: "",
 
-    const [pokemon, setPokemon]=useState({
-        rarity: "basic",
-        name: "",
-        species: "",
-        img: "",
-        hp: "",
-        type: "",
-        icon: "./fire.png",
-        cost: "./normal.png",
-        weakness: "./water.png",
-        description:
-          "Spits fire that is hot enough to melt boulders. known to unintentionally cause forest fires.",
-        abilities: []
-    
-    })
+    abilities: [],
+  });
 
-    const getPokemon = () => {
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
-            (response) => {
-                console.log(response.data);
-                setPokemon({
-                    name: pokemonName,
-                    species: response.data.species.name,
-                    img: response.data.sprites.front_default,
-                    hp: response.data.stats[0].base_stat,
-                    type: response.data.types[0].type.name,
-                    abilities: response.data.abilities,
-                    attack: response.data.stats[1].base_stat,
+  const getPokemon = async () => {
+    const { data } = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const { data: description } = await axios.get(data.species.url);
 
-                })
-                // console.log(pokemon)
-            }
-        )
-    }
+    const  getAbilities = async (abilitiesObj) => {
+      //given an object with a name and a url
+      //i want to save the name, and get description from the URL
+      // i want to return an array of objects with name, description
+
+      const result = await Promise.all(abilitiesObj.map(async (ability) => {
+        const url = ability.ability.url;
+        const { data } = await axios.get(url);
+        return {
+          name: ability.ability.name,
+          description: data.effect_entries[1].short_effect,
+        };
+      }));
+      console.log(result)
+      return result;
+    };
+
+    setPokemon({
+      name: pokemonName,
+      species: data.species.name,
+      img: data.sprites.front_default,
+      hp: data.stats[0].base_stat,
+      type: data.types[0].type.name,
+      abilities: await getAbilities(data.abilities),
+      attack: data.stats[1].base_stat,
+      icon: `./${data.types[0].type.name}.png`,
+      weight: data.weight,
+      description: description.flavor_text_entries[0].flavor_text,
+      shape: description.shape.name,
+    });
+
+    // console.log(pokemon.abilities);
+    // console.log(data);
+    // console.log(description);
+  };
+
   return (
     <div>
       <h1>Create your own pokemon card</h1>
       <input
-      type="text"
-      onChange={(event) => {
-          setPokemonName(event.target.value)
-      }}
-      >
-        
-      </input>
+        type="text"
+        onChange={(event) => {
+          setPokemonName(event.target.value);
+        }}
+      ></input>
       <button onClick={getPokemon}>Search</button>
-      <div>{pokemonName}</div>
-      <img src={pokemon.img}></img>
-      <PokemonCard pokemon={pokemon}/>
+
+      <div className="about">
+        <PokemonCard pokemon={pokemon} />
+      </div>
     </div>
   );
 };
